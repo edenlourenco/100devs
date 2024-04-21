a3c1ec5888a17dd5f71edfced560d12b7ecfe286
joao lourenco <lourencoo@gmail.com>
2024-04-21 16:35:46 +0100

    testing new calculator things
    Now i will make some changes to make sure i learn the conceps right

---
+++ class12-materials/simple-calculator/main.js
@@ -0,0 +1,10 @@
+const total = 0 
+
+document.querySelector('#pumpkin').addEventListener('click',zero)
+document.querySelector('#dominusPizza').addEventListener('click',three)
+
+
+function zero() {
+total = 0
+document.querySelector('#placeToPutResult').innerText = total
+}
