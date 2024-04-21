361cda89d27edbbadc0fed330f5305c165331386
joao lourenco <lourencoo@gmail.com>
2024-04-21 16:59:54 +0100

    calculator_done_alone
    In this i  make an " not so great calculator" from $100devs,  ishould delete the original file and make another one from zero, i guess i success

--- class12-materials/simple-calculator/main.js
+++ class12-materials/simple-calculator/main.js
@@ -2,7 +2,9 @@ let total = 0
 
 document.querySelector('#pumpkin').addEventListener('click', zero)
 document.querySelector('#dominosPizza').addEventListener('click', three)
-
+document.querySelector('#zebra').addEventListener('click' , nine)
+document.querySelector('#cantThinkOfAnything').addEventListener('click', plus2)
+document.querySelector('#testing').addEventListener('click' , menos10)
 
 
 function zero() {
@@ -13,4 +15,19 @@ function zero() {
 function three() {
     total = total + 3
     document.querySelector('#placeToPutResult').innerText = total 
+}
+
+function nine() {
+    total = total +9
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function plus2 () {
+    total = total  - 2 
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function menos10 () {
+    total = total - 10
+    document.querySelector('#placeToPutResult').innerText = total
 }
