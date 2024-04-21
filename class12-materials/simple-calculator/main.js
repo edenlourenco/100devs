27236f221aa53ed6bcc9d541693630a79dfa3977
joao lourenco <lourencoo@gmail.com>
2024-04-21 16:44:31 +0100

    Working_calculator_+3_0
    now iam sure the calculator has working as i inted.

--- class12-materials/simple-calculator/main.js
+++ class12-materials/simple-calculator/main.js
@@ -1,10 +1,16 @@
-const total = 0 
+let total = 0 
 
-document.querySelector('#pumpkin').addEventListener('click',zero)
-document.querySelector('#dominusPizza').addEventListener('click',three)
+document.querySelector('#pumpkin').addEventListener('click', zero)
+document.querySelector('#dominosPizza').addEventListener('click', three)
+
 
 
 function zero() {
-total = 0
-document.querySelector('#placeToPutResult').innerText = total
+    total = 0
+    document.querySelector('#placeToPutResult').innerText = total
+}
+
+function three() {
+    total = total + 3
+    document.querySelector('#placeToPutResult').innerText = total 
 }
